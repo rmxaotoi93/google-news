348a79ad31d414c9a2b9c100c0d4f8716e678372
rmxaotoi93 <cauchu2287>
2020-05-20 21:37:45 +0700

    second news

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 let newList = []
+let page = 1;
 
 const apiKey = "138bc52c706d413db5d0d0bba09b01ac"
-const loadNews = async() =>{
-    let url = `https://newsapi.org/v2/everything?q=bitcoin&from=2020-04-20&sortBy=publishedAt&apiKey=${apiKey}`
+const loadNews = async(status) =>{
+    let url = `https://newsapi.org/v2/everything?q=bitcoin&page=${page}&from=2020-04-20&sortBy=publishedAt&apiKey=${apiKey}`
     let data = await fetch(url);
     let result = await data.json();
     
-    newList = result.articles
     
-    render(newList)
-    document.getElementById('storyAmount').innerHTML = `No of articles: ${newList.length }`
-    console.log(newList)
     
+    if (status == 'firstload' ) {
+        newsList = result.articles;
+        render(newsList);
+    } else if (status == 'loadM') {
+        let dataList = result.articles;
+        newsList = newsList.concat(dataList);
+        
+        render(newsList);
+    }
 }
 
 const render = (list) =>{
-    
+
+    document.getElementById("noNews").innerHTML = list.length;
     let newsHtml = list.map(item => `
     
     <div id="news" class="col-md-5 mb-5">
@@ -38,51 +45,14 @@ const render = (list) =>{
         
     </div>
 
-</div>`).join('') //moment("20111031", "YYYYMMDD").fromNow()
+</div>`).join('') 
 document.getElementById('newsArea').innerHTML = newsHtml 
 
 }
 
-
-let loadMore = async() =>{
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`
-    let data = await fetch(url);
-    let result = await data.json();
-    
-    newList = result.articles
-    render2(newList)
-    document.getElementById('storyAmount').innerHTML = `No of articles: ${newList.length + newList.length}`
-    console.log(newList)
-    
+function loadMore() {
+    page++;
+    loadNews('loadM');
 }
 
-
-
-const render2 = (list) =>{
-    
-    let newsHtml = list.map(item => `
-    
-    <div id="news" class="col-md-5 mb-5">
-
-    <div id="contentsAreaMore" >
-        <div id="title">
-        <a href="${item.url}">${item.title}</a>
-        </div>
-        <div id="source">${item.source.name}</div>
-
-        <div id="publishedAt">${item.publishedAt}</div>
-        <a href="${item.url}">Read More</a>
-    </div>
-
-    <div id="imgAreaMore">
-        <a href="${item.url}">
-        <img src="${item.urlToImage}">
-        </a>
-        
-    </div>
-
-</div>`).join('')
-document.getElementById('loadM').innerHTML = newsHtml
-
-}
-loadNews()
+loadNews('firstload')
